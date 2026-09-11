9a52eb527923e9037079e923ca45e785a077d257
Алексей Вишницкий <alexei.vishnitsky@yandex.ru>
2026-09-11 22:31:03 +0300

    homework_23

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from catalog.models import Product
 
 
 def home(request):
-    return render(request, 'catalog/home.html')
+    latest_products = Product.objects.all()[:5]
+
+
+    print("\n--- ПОСЛЕДНИЕ 5 ПРОДУКТОВ В БАЗЕ ДАННЫХ ---")
+    for product in latest_products:
+        print(f"ID: {product.id} | Название: {product.name} | Цена: {product.price}")
+    print("-------------------------------------------\n")
+
+
+    return render(request, 'catalog/home.html', {'products': latest_products})
 
 
 def contacts(request):
@@ -13,11 +23,10 @@ def contacts(request):
         phone = request.POST.get('phone')
         message = request.POST.get('message')
 
-
         print(f"Новая заявка! Имя: {name}, Телефон: {phone}, Сообщение: {message}")
-
 
         success_message = "Данные успешно отправлены!"
 
     return render(request, 'catalog/contacts.html', {'success_message': success_message})
+
 
