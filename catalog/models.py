e8050532f5870b73902fc44c54ee13cbfc253f54
Алексей Вишницкий <alexei.vishnitsky@yandex.ru>
2026-09-11 22:30:50 +0300

    homework_23

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание', blank=True, null=True)
+    image = models.ImageField(upload_to='products/', verbose_name='Изображение', blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products', verbose_name='Категория')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена за покупку')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = ['-created_at']
+
+
+class ContactInfo(models.Model):
+    phone = models.CharField(max_length=20, verbose_name='Телефон')
+    email = models.EmailField(verbose_name='Email')
+    address = models.TextField(verbose_name='Адрес', blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Контактные данные'
+        verbose_name_plural = 'Контактные данные'
+
+    def __str__(self):
+        return f"Контакты: {self.email}"
